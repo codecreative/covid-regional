4a722929872e42ed442d50ab5a874ef2dbfbbdf1
byron <byron@codecreative.com>
2021-11-22 11:36:41 +0000

    update with saved file

--- vaccines-join-v2.R
+++ vaccines-join-v2.R
@@ -3,23 +3,47 @@
 library('dplyr')
 library(tidyverse)
 
-vax <- read.csv( url("http://ix.cnn.io/data/novel-coronavirus-2019-ncov/vaccines-world/vaccine-owid-latest.csv")) 
+# read in the full vax history
+vax_raw <- read.csv( url("http://ix.cnn.io/data/novel-coronavirus-2019-ncov/vaccines-world/vaccine-owid-history.csv")) 
 
+# filter out the no records for "totalDoses"
+vax <- vax_raw %>%
+  filter(!is.na(totalDoses))
+
+# get the most recent record (of totalDoses) for each country
+vax_latest <- vax %>%
+  group_by(code) %>%
+  arrange(date) %>%
+  slice(n()) %>%
+  ungroup()
+
+# read in the country -> region matchup
 un_regions <- read_csv('un-mod-regions.csv') %>%
   mutate( code = data__alpha3 ) %>%
   select( c('code', 'cnnRegion'))
 
+# join the latest records with the region info
+joined <- vax_latest %>%
+  left_join(un_regions) %>%
+  write_csv( 'with-regions-v2.csv')
 
-joined <- vax %>%
-  left_join(un_regions)
+# read in the population data for regions
+region_pop <- read_csv('region_pop.csv')
 
-grouped <- joined %>%
-  filter(!is.na(totalDosesPer100)) %>%
-  write_csv( 'with-regions.csv')
-
-
-countries_fully_last <- countries_fully %>%
-  group_by(code) %>%
-  arrange(date) %>%
-  slice(n()) %>%
-  select( date, code, name, peopleFullyVaccinatedPer100)
+# add up the totals for each region and get per100 based on the region pop data
+by_continent <- joined %>%
+  select( c('date', 'code', 'name', 'totalDoses', 'cnnRegion') )  %>%
+  group_by( cnnRegion ) %>%
+  summarise(across(
+    .cols = c('totalDoses'), 
+    .fns = sum, 
+    .names = "{col}"
+  )) %>%
+  ungroup() %>%
+  left_join( region_pop ) %>%
+  mutate( 
+    totalDosesPer100 = (totalDoses/pop_est_2020) * 100,
+    totalDosesPer100Rounded = round( totalDosesPer100 )
+  ) %>%
+  arrange( desc(totalDosesPer100Rounded) ) %>%
+  write_csv( 'with-regions-calculated.csv')
