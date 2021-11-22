e7976a2d9e1a3e8e7ddcc0688deb52ae27322a79
byron <byron@codecreative.com>
2021-11-22 11:33:40 +0000

    add new script as potential replacement with calculations included

---
+++ vaccines-join-v2.R
@@ -0,0 +1,25 @@
+
+
+library('dplyr')
+library(tidyverse)
+
+vax <- read.csv( url("http://ix.cnn.io/data/novel-coronavirus-2019-ncov/vaccines-world/vaccine-owid-latest.csv")) 
+
+un_regions <- read_csv('un-mod-regions.csv') %>%
+  mutate( code = data__alpha3 ) %>%
+  select( c('code', 'cnnRegion'))
+
+
+joined <- vax %>%
+  left_join(un_regions)
+
+grouped <- joined %>%
+  filter(!is.na(totalDosesPer100)) %>%
+  write_csv( 'with-regions.csv')
+
+
+countries_fully_last <- countries_fully %>%
+  group_by(code) %>%
+  arrange(date) %>%
+  slice(n()) %>%
+  select( date, code, name, peopleFullyVaccinatedPer100)
